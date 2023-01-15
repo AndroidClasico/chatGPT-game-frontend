faa25e9c80d89b97f1fcbe398d523299883d9d14
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2023-01-15 16:15:20 -0700

    frontend commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import WelcomePage from "./Welcome";
+import "./App.css";
+import { useNavigate, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    console.log(data);
+    setFormSubmitted(true);
+    navigate("/welcome");
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  if (!formSubmitted) {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="text"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Routes>
+          <Route
+            path="/welcome"
+            element={<WelcomePage {...props} username={username} />}
+          />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
